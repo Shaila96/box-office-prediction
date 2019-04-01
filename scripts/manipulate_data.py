6720b77d21d5a196663dc6df4cada11568686a95
Shaila Zaman <shaila.zaman96@gmail.com>
2019-04-01 14:27:40 -0500

    renamed file name

---
+++ scripts/manipulate_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+import ast
+
+def main():
+    train_df = pd.read_csv('../data/tmdb-box-office-prediction/train.csv')
+    test_df = pd.read_csv('../data/tmdb-box-office-prediction/test.csv')
+
+    # print(train_df.columns)
+    # print(train_df.describe())
+    # print(train_df.head())
+
+    # print(train_df.info())
+    train_df['genres'] = train_df['genres'].apply(lambda x: {} if pd.isnull(x) else ast.literal_eval(x))
+    train_df['genres'] = train_df['genres'].apply(lambda x: [i['name'] for i in x] if x != {} else []).values
+    # print(train_df.info())
+    print(train_df['genres'])
+
+
+main()
+
+
