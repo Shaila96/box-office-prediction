085d20be7f6262fb56b73e5d1dcde92aca2ad336
Shaila Zaman <shaila.zaman96@gmail.com>
2019-04-01 13:35:18 -0500

    json to list - genres

--- scripts/visualize_data.py
+++ scripts/visualize_data.py
@@ -1,14 +1,19 @@
 import pandas as pd
+import json
+import ast
 
-train_df = pd.read_csv('../data/tmdb-box-office-prediction/train.csv')
-test_df = pd.read_csv('../data/tmdb-box-office-prediction/test.csv')
+def main():
+    train_df = pd.read_csv('../data/tmdb-box-office-prediction/train.csv')
+    test_df = pd.read_csv('../data/tmdb-box-office-prediction/test.csv')
 
-print("train dataset size:", train_df.shape)
-print("test dataset size: ", test_df.shape)
+    # print(train_df.columns)
+    # print(train_df.describe())
+    # print(train_df.head())
 
+    # print(train_df.info())
+    train_df['genres'] = train_df['genres'].apply(lambda x: {} if pd.isnull(x) else ast.literal_eval(x))
+    train_df['genres'] = train_df['genres'].apply(lambda x: [i['name'] for i in x] if x != {} else []).values
+    # print(train_df.info())
+    print(train_df['genres'])
 
-
-print(train_df.columns)
-print(train_df.describe())
-# print(train_df.head())
-
+main()
